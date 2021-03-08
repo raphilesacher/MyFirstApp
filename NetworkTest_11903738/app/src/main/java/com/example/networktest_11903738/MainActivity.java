29f9b22f03e612139b623cf7f02ab18a8aaea4b2
Raphael Lesacher <r1lesacher@edu.aau.at>
2021-03-08 20:06:11 +0100

    created project NetworkTest_11903738

---
+++ NetworkTest_11903738/app/src/main/java/com/example/networktest_11903738/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.networktest_11903738;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
