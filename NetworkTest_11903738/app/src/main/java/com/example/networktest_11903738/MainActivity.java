2a0464672db6ce046f2bf0644ac8d1cba0a7e52a
Raphael Lesacher <r1lesacher@edu.aau.at>
2021-03-09 15:41:07 +0100

    added TextInputLayout matrInput to MainActivity

--- NetworkTest_11903738/app/src/main/java/com/example/networktest_11903738/MainActivity.java
+++ NetworkTest_11903738/app/src/main/java/com/example/networktest_11903738/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.*;
 
+import com.google.android.material.textfield.TextInputLayout;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -14,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
 
         Button button = findViewById(R.id.button);
         TextView serverResponse = findViewById(R.id.serverResponse);
+        TextInputLayout matrInput = findViewById(R.id.matrInput);
+        
 
 
     }
