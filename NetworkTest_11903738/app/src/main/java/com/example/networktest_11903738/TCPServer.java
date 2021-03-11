e73f05dd127b185735429236d757dcc80fefcce3
Raphael Lesacher <r1lesacher@edu.aau.at>
2021-03-11 14:31:59 +0100

    implemented TCPServer class

--- NetworkTest_11903738/app/src/main/java/com/example/networktest_11903738/TCPServer.java
+++ NetworkTest_11903738/app/src/main/java/com/example/networktest_11903738/TCPServer.java
@@ -1,4 +1,23 @@
 package com.example.networktest_11903738;
+import java.io.*;
+import java.net.*;
 
 public class TCPServer {
+    public static void main(String[] args) throws IOException {
+        String clientSentence;
+        String capitalizedSentence;
+
+        ServerSocket welcomeSocket = new ServerSocket(53212);
+        while(true) {
+            Socket connectionSocket = welcomeSocket.accept();
+
+            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+
+            clientSentence = inFromClient.readLine();
+            capitalizedSentence = clientSentence.toUpperCase() +'\n';
+
+            outToClient.writeBytes(capitalizedSentence);
+        }
+    }
 }
