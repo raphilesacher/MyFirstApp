c1807e73c68083b4f83f213074d18164e147a3e1
Raphael Lesacher <r1lesacher@edu.aau.at>
2021-03-11 14:05:49 +0100

    implemented TCPClient

--- NetworkTest_11903738/app/src/main/java/com/example/networktest_11903738/TCPClient.java
+++ NetworkTest_11903738/app/src/main/java/com/example/networktest_11903738/TCPClient.java
@@ -1,4 +1,22 @@
 package com.example.networktest_11903738;
+import java.io.*;
+import java.net.*;
 
 public class TCPClient {
+    public static void main(String[] args) throws Exception{
+        String toServer;
+        String fromServer;
+
+        BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+        Socket clientSocket = new Socket("se2-isys.aau.at", 53212);
+        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+        BufferedReader serverResponse = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+        toServer = userInput.readLine();
+        outToServer.writeBytes(toServer);
+        fromServer = serverResponse.readLine();
+        System.out.println(fromServer);
+
+        clientSocket.close();
+    }
 }
