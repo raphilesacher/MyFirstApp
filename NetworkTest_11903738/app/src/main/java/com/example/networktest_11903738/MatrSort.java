a7796b191db858ed78cf871dc3b5d60e223d4e9a
Raphael Lesacher <r1lesacher@edu.aau.at>
2021-03-12 12:40:45 +0100

    implemented methods splitList and mergeLists

--- NetworkTest_11903738/app/src/main/java/com/example/networktest_11903738/MatrSort.java
+++ NetworkTest_11903738/app/src/main/java/com/example/networktest_11903738/MatrSort.java
@@ -1,4 +1,4 @@
-/*package com.example.networktest_11903738;
+package com.example.networktest_11903738;
 
 import java.util.ArrayList;
 
@@ -21,10 +21,52 @@ public class MatrSort {
         printList(sorted);
     }
 
-    private void printList(ArrayList<Integer> sorted) {
+    private ArrayList<Integer> splitList(ArrayList<Integer> toSort) {
+        //list for even numbers
+        ArrayList<Integer> evenNums = new ArrayList<>();
+        //list for odd numbers
+        ArrayList<Integer> oddNums = new ArrayList<>();
+
+        //put even numbers in evenNums and odd numbers in oddNums
+        for (int i = 0; i < toSort.size(); i++) {
+            if (toSort.get(i)%2 == 0) {
+                evenNums.add(toSort.get(i));
+            }else {
+                oddNums.add(toSort.get(i));
+            }
+        }
+
+        sortList(evenNums);
+        sortList(oddNums);
+
+        return mergeLists(evenNums, oddNums);
+    }
+    public void sortList(ArrayList<Integer> toSort) {
+        int swap;
+        //BubbleSort Algorithm, very effective ^^
+        for (int i = 0; i < toSort.size()-1; i++) {
+            for (int j = 0; j < toSort.size() - i - 1;  j++) {
+                if (toSort.get(j) > toSort.get(j+1)) {
+                    swap = toSort.get(j);
+                    toSort.set(j, toSort.get(j+1));
+                    toSort.set(j+1, swap);
+                }
+            }
+        }
     }
 
-    private ArrayList<Integer> splitList(ArrayList<Integer> matrList) {
+    public ArrayList<Integer> mergeLists(ArrayList<Integer> evenNums, ArrayList<Integer> oddNums) {
+
+        ArrayList<Integer> mergedList = new ArrayList<>();
+        mergedList.addAll(evenNums);
+        mergedList.addAll(oddNums);
+
+        return mergedList;
     }
+
+    private void printList(ArrayList<Integer> sorted) {
+
+    }
+
+
 }
-*/
